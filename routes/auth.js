2b902edac33ae531a8dc51b5a46f7bbadad0aec1
izaacyo <mariusppetrut@gmail.com>
2021-10-07 21:23:53 +0300

    created model schemas

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const User = require("../models/User")
+
+
+
+//REGISTER 
+
+router.post("/register", (req, res) => {
+    const newUser = new User({
+
+    })
+})
+
+
+module.exports = router
