1b4c41cbfdbd658c94a009d94a3e6aaa848ea60c
izaacyo <mariusppetrut@gmail.com>
2021-10-08 15:18:12 +0300

    created orders route

--- models/Cart.js
+++ models/Cart.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const CartSchema = new mongoose.Scehma(
+const CartSchema = new mongoose.Schema(
     {
         userId: { type: String, required: true },
         products: [
