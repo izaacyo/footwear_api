708440637077fadcda4b0321a9656ec596dfda2e
izaacyo <mariusppetrut@gmail.com>
2021-10-27 23:50:48 +0300

    downgraded node-fetch

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -42,9 +42,9 @@ const sendEmail = (to, url, txt) => {
         subject: "SneakersLoop authentication",
         html: `
             <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-            <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the DevAT channel.</h2>
+            <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to SneakerLoop.</h2>
             <p>Congratulations! You're almost set to start using SneakersLoop.
-                Just click the button below to validate your email address.
+                Just click the button below.
             </p>
             
             <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">${txt}</a>
